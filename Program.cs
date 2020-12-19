ec768a3805fa6a8a453d164ceb19990cf2e707ab
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-12-19 14:40:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7
+{
+    interface emp
+    {
+        void show();
+
+
+       
+    }
+
+    interface emp2
+    {
+         void show();
+
+
+    }
+
+    class Engineer : emp,emp2
+    {
+         void emp.show()
+        {
+            Console.WriteLine("hello");
+        }
+
+         void emp2.show()
+        {
+            Console.WriteLine("hey");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engineer k = new Engineer();
+            emp2 test = k;
+            emp p = k;
+            test.show();
+            p.show();
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
