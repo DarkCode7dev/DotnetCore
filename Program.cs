8e1a3e1de54ce390fa35b9c45f74633784e2a5a5
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-12-26 13:58:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9thday
+{
+
+    static class one
+    {
+        static int a = 12;
+        static int b = 12;
+        static int s;
+        public static void add()
+        {
+            s = a + b;
+            Console.WriteLine(s);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            one.add();
+          
+             Console.ReadLine();
+        }
+    }
+
+   
+}
