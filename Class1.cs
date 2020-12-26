9c1c0e4014ca491c13646f7e033127d77f2e3e8f
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-12-26 10:08:59 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideOne
+{
+    public class Class1
+    {
+        public void show()
+        {
+            Console.WriteLine("Go Corona Go!");
+        }
+    }
+}
