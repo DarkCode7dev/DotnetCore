f8c7b4797794ef0413be4de603c34bf07e070a50
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2020-12-26 13:45:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9thday
+{
+
+    class one
+    {
+        int height;
+        int radius;
+
+        public one(int x, int y)
+        {
+            height = x;
+            radius = y;
+        }
+
+        public void volume()
+        {
+            double result = Math.PI * radius * radius * height;
+            Console.WriteLine(result);
+        }
+
+        public void surface()
+        {
+            double t = 2 * Math.PI * radius * height;
+            Console.WriteLine(t);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            one k = new one(1,5);
+            k.volume();
+            k.surface();
+             Console.ReadLine();
+        }
+    }
+
+   
+}
