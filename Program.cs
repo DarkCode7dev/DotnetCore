60a428da387266526cb6b386fc26756fa88d5f8e
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-09 14:06:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList k = new SortedList();
+            k.Add(40, "ravi");
+            k.Add(112, "mohan");
+            k.Add(567, "Praveen");
+            k.Add(100, "ram");
+            k.Add(102, "harshit");
+
+            foreach (DictionaryEntry s in k)
+            {
+                Console.WriteLine(s.Key + ":" + s.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
