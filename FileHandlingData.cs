e53b222311351a692a82dea070e7b93c05245acb
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-10 09:11:01 +0530

    Update and rename Program.cs to FileHandlingData.cs

--- FileHandlingData.cs
+++ FileHandlingData.cs
@@ -10,26 +10,14 @@ namespace day14
     {
         static void Main(string[] args)
         {
-            //string s = @"Desktop\\batman.txt";
-            //string s2 = @"Desktop\\batch4.txt";
-
-            //try
-            //{
-            //    File.Copy(s, s2, true);
-            //}
-            //catch (IOException e)
-            //{
-
-            //    Console.WriteLine(e);
-            //}
-
+           
             DirectoryInfo di = new DirectoryInfo(@"f:\5 sem");
             foreach (DirectoryInfo dir in di.GetDirectories())
             {
-                //foreach (FileInfo s in dir.GetFiles())
-                //{
-                //    Console.WriteLine(s);
-                //}
+                foreach (FileInfo s in dir.GetFiles())
+                {
+                    Console.WriteLine(s);
+                }
                 Console.WriteLine(dir);
             }
         }
