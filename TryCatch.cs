c2fc9d4264178ba81f79644d0d406434f0bce0b3
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-10 09:36:55 +0530

    Update and rename Program.cs to TryCatch.cs

--- TryCatch.cs
+++ TryCatch.cs
@@ -10,20 +10,7 @@ namespace day14
     {
         static void Main(string[] args)
         {
-            //string s = @"F\\co5\\batman.txt";
-            //string s2 = @"F\\co5\\batch4.txt";
-
-            //File.Copy(s, s2, true);
-
-            //DirectoryInfo di = new DirectoryInfo(@"f:\5 sem");
-            //foreach (DirectoryInfo dir in di.GetDirectories())
-            //{
-            //    foreach (FileInfo s in dir.GetFiles())
-            //    {
-            //        s.Delete();
-            //    }
-            //    dir.Delete();
-            //}
+           
 
             try
             {
