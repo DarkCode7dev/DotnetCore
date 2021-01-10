7f12db9eabed42d939a9721a51cd28ace29a4d5a
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-10 09:08:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = @"Desktop\\batman.txt";
+            //string s2 = @"Desktop\\batch4.txt";
+
+            //try
+            //{
+            //    File.Copy(s, s2, true);
+            //}
+            //catch (IOException e)
+            //{
+
+            //    Console.WriteLine(e);
+            //}
+
+            DirectoryInfo di = new DirectoryInfo(@"f:\5 sem");
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                //foreach (FileInfo s in dir.GetFiles())
+                //{
+                //    Console.WriteLine(s);
+                //}
+                Console.WriteLine(dir);
+            }
+        }
+    }
+}
