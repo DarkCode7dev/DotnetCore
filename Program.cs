4fff94b60ff052f53864bb9d3dfe08715a0931f0
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-17 09:07:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    class one
+    {
+        public void div(int x,int y)
+        {
+            if (y==2)
+            {
+                throw new Exception("you can not divide by 2");
+            }
+            else
+            {
+                int z;
+                z = x / y;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            one k = new one();
+            try
+            {
+                k.div(10, 2);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("hello");
+            Console.ReadLine();
+        }
+    }
+}
