b48da7f98259c61ee07cc90cd11180a6835b2020
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-17 09:53:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+   partial class Manager{
+        public void show()
+        {
+            Console.WriteLine("I am Manager");
+        }
+
+    }
+
+    partial class Manager
+    {
+        public void show2()
+        {
+            Console.WriteLine("I am Manager2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Manager k = new Manager();
+            k.show();
+            k.show2();
+           
+            Console.ReadLine();
+        }
+    }
+}
