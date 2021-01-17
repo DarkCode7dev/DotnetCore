08a96acfc03e077183ac00c7b85e58fc0e4baed2
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-01-17 09:31:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int x = 10;
+                int y = 0;
+                int z=0;
+                z = x / y;
+                Console.WriteLine(z);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("hello");
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
